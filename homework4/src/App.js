471dcb1a3efdf1d22b9505e990eaf92750671584
AKKatung159 <6434496423@student.chula.ac.th>
2023-11-19 14:40:28 +0700

    add 4G and 5G

--- homework4/src/App.js
+++ homework4/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const handleCombine = () => {
+    setCombined(Number(input1) + Number(input2));
+  }
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [combined, setCombined] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="number" placeholder='enter the first number' value={input1} onChange={(e) => setInput1(e.target.value)} />
+      <input type="number" placeholder='enter the second number' value={input2} onChange={(e) => setInput2(e.target.value)} />
+      <button onClick={handleCombine}>Combine</button>
+      <input type="number" placeholder='here is sum' value={combined} readOnly />
     </div>
   );
 }
-
 export default App;
